3296aba2a2016385c1e6ac93282d64d49c648d94
Michael Irlam <michaelirlam@hotmail.co.uk>
2026-08-29 22:04:50 +0100

    added LeafNode child class and tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -19,3 +19,17 @@ class HTMLNode:
 
     def __repr__(self):
         return f"{self.tag}, {self.value}, {self.children}, {self.props}"
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag=tag, value=value, props=props)
+
+    def to_html(self):
+        if not self.value:
+            raise ValueError
+        if not self.tag:
+            return self.value
+        return f"<{self.tag}>{self.value}</{self.tag}>"
+
+    def __repr__(self):
+        return f"{self.tag}, {self.value}, {self.props}"
---
+++ src/test_leafnode.py
@@ -0,0 +1,15 @@
+import unittest
+from htmlnode import LeafNode
+
+class TestLeafNode(unittest.TestCase):
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_no_value(self):
+        node = LeafNode("p", None)
+        self.assertRaises(ValueError, node.to_html)
+
+    def test_leaf_to_html_no_tag(self):
+        node = LeafNode(None, "Hello, world!")
+        self.assertEqual(node.to_html(), "Hello, world!")
