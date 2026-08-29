8026bcb34cb43daba5a3060b5c12d8bf576e655c
Michael Irlam <michaelirlam@hotmail.co.uk>
2026-08-29 14:16:58 +0100

    created Enum and TextNode classes and basic methods

--- src/main.py
+++ src/main.py
@@ -1,1 +1,7 @@
-print("hello world")
+from textnode import TextNode, TextType
+
+def main():
+    text_node = TextNode("hello world", TextType.BOLD, "https://www.boot.dev")
+    print(text_node)
+
+main()
