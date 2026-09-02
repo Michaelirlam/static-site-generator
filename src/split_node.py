77b76c26142e1cc32833b08734e75dcadf2849cc
Michael Irlam <michaelirlam@hotmail.co.uk>
2026-09-02 19:31:25 +0100

    added logic for converting links and images and unit tests

--- src/split_node.py
+++ src/split_node.py
@@ -1,4 +1,5 @@
 from textnode import TextNode, TextType
+from matches import extract_markdown_images, extract_markdown_links
 
 def split_nodes_delimiter(old_nodes:list[TextNode], delimiter:str, text_type:TextType) -> list[TextNode]:
     new_nodes = []
@@ -19,4 +20,56 @@ def split_nodes_delimiter(old_nodes:list[TextNode], delimiter:str, text_type:Tex
                     else:
                         new_node = TextNode(split_node[i], text_type)
                         new_nodes.append(new_node)
+    return new_nodes
+
+def split_nodes_image(old_nodes: list[TextNode]) -> list[TextNode]:
+    new_nodes = []
+    for node in old_nodes:
+        if node.text == "":
+            continue
+        if node.text_type is not TextType.TEXT:
+            new_nodes.append(node)
+        elif extract_markdown_images(node.text) == []:
+            new_nodes.append(node)
+        else:
+            image_tuples = extract_markdown_images(node.text)
+            remaining_text = node.text
+            for image in image_tuples:
+                alt_text, url = image[0], image[1]
+                split = remaining_text.split(f"![{alt_text}]({url})", 1)
+                if split[0] != "":
+                    text_node = TextNode(split[0], TextType.TEXT)
+                    new_nodes.append(text_node)
+                image_node = TextNode(alt_text, TextType.IMAGE, url)
+                new_nodes.append(image_node)
+                remaining_text = split[1]
+            if len(remaining_text) != 0:
+                text_node = TextNode(remaining_text, TextType.TEXT)
+                new_nodes.append(text_node)
+    return new_nodes
+
+def split_nodes_link(old_nodes: list[TextNode]) -> list[TextNode]:
+    new_nodes = []
+    for node in old_nodes:
+        if node.text == "":
+            continue
+        if node.text_type is not TextType.TEXT:
+            new_nodes.append(node)
+        elif extract_markdown_links(node.text) == []:
+            new_nodes.append(node)
+        else:
+            link_tuples = extract_markdown_links(node.text)
+            remaining_text = node.text
+            for link in link_tuples:
+                alt_text, url = link[0], link[1]
+                split = remaining_text.split(f"[{alt_text}]({url})", 1)
+                if split[0] != "":
+                    text_node = TextNode(split[0], TextType.TEXT)
+                    new_nodes.append(text_node)
+                link_node = TextNode(alt_text, TextType.LINK, url)
+                new_nodes.append(link_node)
+                remaining_text = split[1]
+            if len(remaining_text) != 0:
+                text_node = TextNode(remaining_text, TextType.TEXT)
+                new_nodes.append(text_node)
     return new_nodes
--- src/test_split_node.py
+++ src/test_split_node.py
@@ -1,6 +1,6 @@
 import unittest
 from textnode import TextType, TextNode
-from split_node import split_nodes_delimiter
+from split_node import split_nodes_delimiter, split_nodes_image, split_nodes_link
 
 class TestSplitNode(unittest.TestCase):
     def test_not_text_node(self):
@@ -90,3 +90,165 @@ class TestSplitNode(unittest.TestCase):
         node = TextNode("", TextType.TEXT)
         new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
         self.assertEqual(new_nodes, [])
+
+class SplitImages(unittest.TestCase):
+    def test_empty_string(self):
+        node = TextNode("", TextType.TEXT)
+        new_nodes = split_nodes_image([node])
+        self.assertEqual(new_nodes, [])
+
+    def test_non_text_type(self):
+        node = TextNode("I am a **bold node**", TextType.BOLD)
+        new_nodes = split_nodes_image([node])
+        expected = [node]
+        self.assertEqual(new_nodes, expected)
+
+    def test_single_image_no_text_single_node(self):
+        node = TextNode("![image](url)", TextType.TEXT)
+        new_nodes = split_nodes_image([node])
+        expected = [
+            TextNode("image", TextType.IMAGE, "url")
+        ]
+        self.assertEqual(new_nodes, expected)
+
+    def test_single_image_leading_text_single_node(self):
+        node = TextNode("This is an ![image](url)", TextType.TEXT)
+        new_nodes = split_nodes_image([node])
+        expected = [
+            TextNode("This is an ", TextType.TEXT),
+            TextNode("image", TextType.IMAGE, "url")
+        ]
+        self.assertEqual(new_nodes, expected)
+
+    def test_single_image_trailing_text_single_node(self):
+        node = TextNode("![image](url) this is an image", TextType.TEXT)
+        new_nodes = split_nodes_image([node])
+        expected = [
+            TextNode("image", TextType.IMAGE, "url"),
+            TextNode(" this is an image", TextType.TEXT)
+        ]
+        self.assertEqual(new_nodes, expected)
+
+    def test_single_image_leading_and_trailing_text_single_node(self):
+        node = TextNode("This is an ![image](url) with trailing text", TextType.TEXT)
+        new_nodes = split_nodes_image([node])
+        expected = [
+            TextNode("This is an ", TextType.TEXT),
+            TextNode("image", TextType.IMAGE, "url"),
+            TextNode(" with trailing text", TextType.TEXT)
+        ]
+        self.assertEqual(new_nodes, expected)
+
+    def test_muliple_image_no_text_single_node(self):
+        node = TextNode("![image1](url)![image2](url)", TextType.TEXT)
+        new_nodes = split_nodes_image([node])
+        expected = [
+            TextNode("image1", TextType.IMAGE, "url"),
+            TextNode("image2", TextType.IMAGE, "url")
+        ]
+        self.assertEqual(new_nodes, expected)
+
+    def test_muliple_image_text_inbetween_single_node(self):
+        node = TextNode("![image1](url) inbetween ![image2](url)", TextType.TEXT)
+        new_nodes = split_nodes_image([node])
+        expected = [
+            TextNode("image1", TextType.IMAGE, "url"),
+            TextNode(" inbetween ", TextType.TEXT),
+            TextNode("image2", TextType.IMAGE, "url")
+        ]
+        self.assertEqual(new_nodes, expected)
+
+    def test_multiple_nodes(self):
+        node1 = TextNode("![image1](url) inbetween ![image2](url)", TextType.TEXT)
+        node2 = TextNode("This is an ![image](url) with trailing text", TextType.TEXT)
+        new_nodes = split_nodes_image([node1, node2])
+        expected = [
+            TextNode("image1", TextType.IMAGE, "url"),
+            TextNode(" inbetween ", TextType.TEXT),
+            TextNode("image2", TextType.IMAGE, "url"),
+            TextNode("This is an ", TextType.TEXT),
+            TextNode("image", TextType.IMAGE, "url"),
+            TextNode(" with trailing text", TextType.TEXT)
+        ]
+        self.assertEqual(new_nodes, expected)
+
+class SplitLinks(unittest.TestCase):
+    def test_empty_string(self):
+            node = TextNode("", TextType.TEXT)
+            new_nodes = split_nodes_link([node])
+            self.assertEqual(new_nodes, [])
+    
+    def test_non_text_type(self):
+        node = TextNode("I am a **bold node**", TextType.BOLD)
+        new_nodes = split_nodes_link([node])
+        expected = [node]
+        self.assertEqual(new_nodes, expected)
+
+    def test_single_link_no_text_single_node(self):
+        node = TextNode("[alt_text](url)", TextType.TEXT)
+        new_nodes = split_nodes_link([node])
+        expected = [
+            TextNode("alt_text", TextType.LINK, "url")
+        ]
+        self.assertEqual(new_nodes, expected)
+
+    def test_single_link_leading_text_single_node(self):
+        node = TextNode("This is a link [alt_text](url)", TextType.TEXT)
+        new_nodes = split_nodes_link([node])
+        expected = [
+            TextNode("This is a link ", TextType.TEXT),
+            TextNode("alt_text", TextType.LINK, "url")
+        ]
+        self.assertEqual(new_nodes, expected)
+
+    def test_single_link_trailing_text_single_node(self):
+        node = TextNode("[alt_text](url) this is a link", TextType.TEXT)
+        new_nodes = split_nodes_link([node])
+        expected = [
+            TextNode("alt_text", TextType.LINK, "url"),
+            TextNode(" this is a link", TextType.TEXT)
+        ]
+        self.assertEqual(new_nodes, expected)
+
+    def test_single_link_leading_and_trailing_text_single_node(self):
+        node = TextNode("This is a link [alt_text](url) with trailing text", TextType.TEXT)
+        new_nodes = split_nodes_link([node])
+        expected = [
+            TextNode("This is a link ", TextType.TEXT),
+            TextNode("alt_text", TextType.LINK, "url"),
+            TextNode(" with trailing text", TextType.TEXT)
+        ]
+        self.assertEqual(new_nodes, expected)
+
+    def test_muliple_link_no_text_single_node(self):
+        node = TextNode("[alt_text1](url)[alt_text2](url)", TextType.TEXT)
+        new_nodes = split_nodes_link([node])
+        expected = [
+            TextNode("alt_text1", TextType.LINK, "url"),
+            TextNode("alt_text2", TextType.LINK, "url")
+        ]
+        self.assertEqual(new_nodes, expected)
+
+    def test_muliple_link_text_inbetween_single_node(self):
+        node = TextNode("[alt_text1](url) inbetween [alt_text2](url)", TextType.TEXT)
+        new_nodes = split_nodes_link([node])
+        expected = [
+            TextNode("alt_text1", TextType.LINK, "url"),
+            TextNode(" inbetween ", TextType.TEXT),
+            TextNode("alt_text2", TextType.LINK, "url")
+        ]
+        self.assertEqual(new_nodes, expected)
+#
+    def test_multiple_nodes(self):
+        node1 = TextNode("[alt_text1](url) inbetween [alt_text2](url)", TextType.TEXT)
+        node2 = TextNode("This is an [alt_text](url) with trailing text", TextType.TEXT)
+        new_nodes = split_nodes_link([node1, node2])
+        expected = [
+            TextNode("alt_text1", TextType.LINK, "url"),
+            TextNode(" inbetween ", TextType.TEXT),
+            TextNode("alt_text2", TextType.LINK, "url"),
+            TextNode("This is an ", TextType.TEXT),
+            TextNode("alt_text", TextType.LINK, "url"),
+            TextNode(" with trailing text", TextType.TEXT)
+        ]
+        self.assertEqual(new_nodes, expected)
