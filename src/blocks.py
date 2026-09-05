88ef6a4bca75edc85baa7728e944b9adf65cc238
Michael Irlam <michaelirlam@hotmail.co.uk>
2026-09-05 14:35:56 +0100

    created logic for converting markdown to blocks and unit tests

--- src/blocks.py
+++ src/blocks.py
@@ -1,5 +1,30 @@
-def markdown_to_blocks(markdown):
+from enum import Enum
+import re
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
+def markdown_to_blocks(markdown: str) -> list[str]:
     blocks = markdown.split("\n\n")
     blocks = [b.strip() for b in blocks]
     blocks = [b for b in blocks if b != ""]
     return blocks
+
+def block_to_block_type(block: str) -> BlockType:
+    if re.match(r"^#{1,6} ", block):
+        return BlockType.HEADING
+    elif block.startswith("```") and block.endswith("```"):
+        return BlockType.CODE
+    elif block.startswith(">"):
+        return BlockType.QUOTE
+    elif block.startswith("- "):
+        return BlockType.UNORDERED_LIST
+    elif re.match(r"^\d+\. ", block):
+        return BlockType.ORDERED_LIST
+    else:
+        return BlockType.PARAGRAPH
--- src/test_blocks.py
+++ src/test_blocks.py
@@ -1,5 +1,5 @@
 import unittest
-from blocks import markdown_to_blocks
+from blocks import markdown_to_blocks, block_to_block_type, BlockType
 
 class TestMarkDownToBlocks(unittest.TestCase):
     def test_markdown_to_blocks(self):
@@ -55,4 +55,131 @@ This is the same paragraph on a new line
         self.assertEqual(
             markdown_to_blocks(md),
             ["First block", "Second block"],
+        )
+
+class TestBlockToBlockType(unittest.TestCase):
+    def test_heading_one(self):
+        block = "# I am a H1"
+        self.assertEqual(
+            block_to_block_type(block),
+            BlockType.HEADING
+        )
+
+    def test_heading_two(self):
+        block = "## I am a H2"
+        self.assertEqual(
+            block_to_block_type(block),
+            BlockType.HEADING
+        )
+
+    def test_heading_three(self):
+        block = "### I am a H3"
+        self.assertEqual(
+            block_to_block_type(block),
+            BlockType.HEADING
+        )
+
+    def test_heading_four(self):
+        block = "#### I am a H4"
+        self.assertEqual(
+            block_to_block_type(block),
+            BlockType.HEADING
+        )
+
+    def test_heading_five(self):
+        block = "##### I am a H5"
+        self.assertEqual(
+            block_to_block_type(block),
+            BlockType.HEADING
+        )
+
+    def test_heading_six(self):
+        block = "###### I am a H6"
+        self.assertEqual(
+            block_to_block_type(block),
+            BlockType.HEADING
+        )
+
+    def test_heading_seven_is_not_heading(self):
+        block = "####### I am not a heading"
+        self.assertEqual(
+            block_to_block_type(block),
+            BlockType.PARAGRAPH
+        )
+
+    def test_hash_without_space_is_not_heading(self):
+        block = "#This is not a heading"
+        self.assertEqual(
+            block_to_block_type(block),
+            BlockType.PARAGRAPH
+        )
+    
+    def test_is_code(self):
+        block = "```This is code```"
+        self.assertEqual(
+            block_to_block_type(block),
+            BlockType.CODE
+        )
+
+    def test_is_not_code(self):
+        block = "```This is not code"
+        self.assertEqual(
+            block_to_block_type(block),
+            BlockType.PARAGRAPH
+        )
+
+    def test_quote(self):
+        block = ">This is a quote"
+        self.assertEqual(
+            block_to_block_type(block),
+            BlockType.QUOTE
+        )
+
+    def test_is_unordered_list(self):
+        block = "- This is an unordered list"
+        self.assertEqual(
+            block_to_block_type(block),
+            BlockType.UNORDERED_LIST
+        )
+
+    def test_is_not_unordered_list(self):
+        block = "-This is not an unordered list"
+        self.assertEqual(
+            block_to_block_type(block),
+            BlockType.PARAGRAPH
+        )
+
+    def test_is_an_ordered_list(self):
+        block = "1. This is not an ordered list"
+        self.assertEqual(
+            block_to_block_type(block),
+            BlockType.ORDERED_LIST
+        )
+
+    def test_is_not_an_ordered_list(self):
+        block = "1.This is not an ordered list"
+        self.assertEqual(
+            block_to_block_type(block),
+            BlockType.PARAGRAPH
+        )
+
+    def test_two_digit_ordered_list(self):
+        block = "10. This is an ordered list"
+        self.assertEqual(
+            block_to_block_type(block),
+            BlockType.ORDERED_LIST
+        )
+
+    def test_paragraph(self):
+        block = "This is a normal paragraph"
+        self.assertEqual(
+            block_to_block_type(block),
+            BlockType.PARAGRAPH
+        )
+
+    def test_empty_block(self):
+        block = ""
+        self.assertEqual(
+            block_to_block_type(block),
+            BlockType.PARAGRAPH
         )
